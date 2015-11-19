0fc1bfcd52d5553b58f4243d40d1abd47ebf50ea
Eddie Reynolds <Edd@eddies-mbp.spelman.edu>
2015-11-19 15:36:02 -0500

    Initial Commit

---
+++ location trackerTests/location_trackerTests.swift
@@ -0,0 +1,36 @@
+//
+//  location_trackerTests.swift
+//  location trackerTests
+//
+//  Created by Eddie Reynolds on 11/19/15.
+//  Copyright © 2015 Eddie Reynolds. All rights reserved.
+//
+
+import XCTest
+@testable import location_tracker
+
+class location_trackerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
